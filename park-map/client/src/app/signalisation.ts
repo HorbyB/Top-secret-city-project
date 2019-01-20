1d4261a46178462089a3aa5305ffe8add0952ab5
Horby Brisseau <horbyb22@gmail.com>
2019-01-20 08:07:15 -0500

    Horby Brisseau : modification de la class Signalisation (ajout d'accesseurs)

--- park-map/client/src/app/signalisation.ts
+++ park-map/client/src/app/signalisation.ts
@@ -5,4 +5,24 @@ export class Signalisation {
   private type: string;
   private geometry: Geometry;
   private properties: Properties;
+
+  public constructor(type: string, geometry: Geometry, Properties: Properties) {
+    this.type = type;
+    this.geometry = geometry;
+    this.properties = properties;
+
+  }
+
+  public getLocalisationLongitue(): number {
+    return this.geometry.getLongitue();
+  }
+
+  public getLocalisationLatitude(): number {
+    return this.geometry.getLatitude();
+  }
+
+  public getCode(): string {
+    return this.properties.getCode()
+  }
+
 }

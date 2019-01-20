9237b6b357cabd5b0f54cdabcd43c5ef89e96585
Horby Brisseau <horbyb22@gmail.com>
2019-01-20 07:32:19 -0500

    Horby Brisseau : modification de la classe Signalisation

--- park-map/client/src/app/signalisation.ts
+++ park-map/client/src/app/signalisation.ts
@@ -1,6 +1,8 @@
-export interface Signalisation {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+import {Geometry} from "./geometry";
+import {Properties} from "./properties";
+
+export class Signalisation {
+  private type: string;
+  private geometry: Geometry;
+  private properties: Properties;
 }

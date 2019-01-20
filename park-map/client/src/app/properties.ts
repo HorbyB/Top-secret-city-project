38ca3c70b7a053ce3c6c51bfad088b8f19233b3e
Horby Brisseau <horbyb22@gmail.com>
2019-01-20 07:31:24 -0500

    Horby Brisseau :  ajout d'une interface Properties

---
+++ park-map/client/src/app/properties.ts
@@ -0,0 +1,45 @@
+export interface Properties {
+  POTEAU_ID_POT: number;
+  PANNEAU_ID_PAN: number;
+  PANNEAU_ID_RPA: number;
+  DESCRIPTION_RPA: string;
+  CODE_RPA: string;
+  FLECHE_PAN: number;
+  TOPONYME_PAN: string;
+  POTEAU_VERSION_POT: number; //
+  DATE_CONCEPTION_POT: number; //
+  PAS_SUR_RUE: string; //
+  DESCRIPTION_REP: string; //
+  DESCRIPTION_RTP: string; //
+  X_EPSG32188: number;
+  Y_EPSG32188: number;
+  Longitude: number;
+  Latitude: number;
+  DESCRIPTION_CAT: string;
+  NOM_ARR: string;
+
+}
+
+/*
+export class Properties {
+  private POTEAU_ID_POT: number;
+  private PANNEAU_ID_PAN: number;
+  private PANNEAU_ID_RPA: number;
+  private DESCRIPTION_RPA: string;
+  private CODE_RPA: string;
+  private FLECHE_PAN: number;
+  private TOPONYME_PAN: string;
+  private POTEAU_VERSION_POT: number; //
+  private DATE_CONCEPTION_POT: number; //
+  private PAS_SUR_RUE: string; //
+  private DESCRIPTION_REP: string; //
+  private DESCRIPTION_RTP: string; //
+  private X_EPSG32188: number;
+  private Y_EPSG32188: number;
+  private Longitude: number;
+  private Latitude: number;
+  private DESCRIPTION_CAT: string;
+  private NOM_ARR: string;
+
+}
+*/
